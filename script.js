0c71115ff2991a1ca94c61c1f0a18cb7c99c4b24
David Pérez <136135434+DavidFPR@users.noreply.github.com>
2025-07-28 18:57:29 -0600

    Añade reglas de responsividad para dispositivos en modo paisaje y desktops

--- script.js
+++ script.js
@@ -64,12 +64,34 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  loop: false,
+  loop: true,
   // If we need pagination
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+
+  autoplay: {
+    delay: 2500,
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
   },
 });
